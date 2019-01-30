9aa93ffc1eff04e934f38f6d51bcef7d4bb765b0
lcw68 <40571490+lcw68@users.noreply.github.com>
2019-01-30 23:56:21 +0800

    Update EXP.cpp

--- EXP.cpp
+++ EXP.cpp
@@ -18,16 +18,16 @@ long double l1_c (double t1, double t2, int k, int m, double adjj = 0)
 {
 	return(exp(k *log(t1) + m *log(t2) - adjj));
 }
-long double l1_AC (double t1, double t2, int x, int y, int a0, int a1, int a2, int k, int m, double adjj = 0) 
+long double l1_AC (double t1, double t2, int x, int y, double a0, double a1, double a2, int k, int m, double adjj = 0) 
 {
 	return(exp(lgamma(a1 + k)- lgamma(k+1)- lgamma(a1) + lgamma(x + y + a0 -m -k) - lgamma(x -k +1) - lgamma(a0 + y - m) 
                                    + lgamma(m + a2) - lgamma(m+1) - lgamma(a2) + lgamma(y +a0 -m) - lgamma(y -m +1) - lgamma(a0) + k *log(t1) + m *log(t2) - adjj));
 }
-long double l2_A (int x, int a0, int a1, int a2, int k, double adjj = 0) 
+long double l2_A (int x, double a0, double a1, double a2, int k, double adjj = 0) 
 {
 	return(exp(lgamma(x +a0 -k) + lgamma(k + a1) - lgamma(a0) - lgamma(x-k+1) - lgamma(a1) - lgamma(k+1) - adjj));
 }
-long double l3_A (int y, int a0, int a1, int a2, int m, double adjj = 0) 
+long double l3_A (int y, double a0, double a1, double a2, int m, double adjj = 0) 
 {
 	return(exp(lgamma(y +a0 -m) + lgamma(m + a2) - lgamma(a0) - lgamma(y-m+1) - lgamma(a2) - lgamma(m+1) - adjj));
 }
@@ -84,7 +84,7 @@ double dnbinom(int k,int r, double p)
 {
 	return(exp(lgamma(r + k) - lgamma(r) - lgamma(k+1)) * po(p, r) * po((1-p), k));
 }
-long double *dBvZINB4_Expt(int x, int y, int a0, int a1, int a2, int b1, int b2, double p1, double p2, double p3, double p4) 
+long double *dBvZINB4_Expt(int x, int y, double a0, double a1, double a2, double b1, double b2, double p1, double p2, double p3, double p4) 
 {
 	double t1 = (float)(b1 + b2 + 1) /(b1 + 1);
 	double t2 = (float)(b1 + b2 + 1) /(b2 + 1);
@@ -93,24 +93,23 @@ long double *dBvZINB4_Expt(int x, int y, int a0, int a1, int a2, int b1, int b2,
 	double adj_C = 0;
 	double adj_sum = 0;
 	long double l1_B = - (+x+y+a0)*log(1 + b1 + b2) + x * log(b1) + y * log(b2) - a1 * log(1 + b1) - a2 * log(1 + b2);
-	long double l2_B = y==0?1:0 * exp(- (x + a0 + a1)*log(1 + b1) + x * log(b1) + adj_B1) * p2 ;
-	long double l3_B = x==0?1:0 * exp(- (y + a0 + a2)*log(1 + b2) + y * log(b2) + adj_B1) * p3 ;
-	long double l4_B = p4 * (x+y==0)?1:0 * exp(adj_B1);
-
-	long double sum_A = 0,sum_AC = 0,sum_A_mat = 0,sum_C_mat = 0;
+	long double l2_B = (y==0?1:0)*1.0 * exp(- (x + a0 + a1)*log(1 + b1) + x * log(b1) + adj_B1) * p2 ;
+	long double l3_B = (x==0?1:0) *1.0* exp(- (y + a0 + a2)*log(1 + b2) + y * log(b2) + adj_B1) * p3 ;
+	long double l4_B = p4 * ((x+y==0)?1:0)*1.0* exp(adj_B1);
+	long double sum_AC = 0,sum_A_mat = 0,sum_C_mat = 0;
 	for(int i = 0;i <= x;i++)
 	{
 		for(int j = 0;j <= y;j++)
 		{
 			l_A_mat[i][j] = l1(x,y,a0,a1,a2,i,j,adj_A);
 			l_C_mat[i][j] = l1_c(t1,t2,i,j,adj_C);
-			sum_A_mat = sum_A_mat + l_A_mat[i][j];
+			sum_A_mat += l_A_mat[i][j];
 			sum_C_mat += l_C_mat[i][j];
 			//cout << i <<" "<<j<<" "<< l_A_mat[i][j] << endl;
 			//system("pause");
 		}
 	}
-	
+	//cout << sum_A_mat << "A"<<sum_C_mat<<"C"<<endl;
 	
 	if (l1_B < -200 && log(l2_B + l3_B + l4_B) < 0)
 	{
@@ -118,7 +117,7 @@ long double *dBvZINB4_Expt(int x, int y, int a0, int a1, int a2, int b1, int b2,
 	    l1_B = l1_B + adj_B1;
 	}
 	l1_B = exp(l1_B) * p1;
-	cout << l1_B <<endl;
+	//cout << l1_B <<endl;
 	
 	while (log(sum_A_mat) > 250) 
 	{
@@ -139,7 +138,7 @@ long double *dBvZINB4_Expt(int x, int y, int a0, int a1, int a2, int b1, int b2,
 	}
 	for(int j = 0;j <= y;j++)
 	{
-		l3_A_mat[j] = l3_A(y, a0, a1, a2, j, adj_A) ;
+		l3_A_mat[j] = l3_A(y, a0, a1, a2, j, adj_A);
 	}
 	while (log(sum_C_mat) > 250) 
 	{
@@ -166,7 +165,7 @@ long double *dBvZINB4_Expt(int x, int y, int a0, int a1, int a2, int b1, int b2,
 		adj_A = adj_A + 100;
     	adj_C = adj_C + 100;
     	sum_AC = 0;
-		sum_A = 0; 
+		sum_A_mat = 0; 
     	for(int i = 0;i <= x;i++)
 		{
 			for(int j = 0;j <= y;j++)
@@ -174,7 +173,7 @@ long double *dBvZINB4_Expt(int x, int y, int a0, int a1, int a2, int b1, int b2,
 				l_A_mat[i][j] = l1(x,y,a0,a1,a2,i,j,adj_A);  
 				l_C_mat[i][j] = l1_c(t1,t2,i,j,adj_C);  
 				sum_AC += l_A_mat[i][j]*l_C_mat[i][j];	
-				sum_A +=  l_A_mat[i][j]; 
+				sum_A_mat +=  l_A_mat[i][j]; 
 			}
 	    }
 	}
@@ -183,7 +182,7 @@ long double *dBvZINB4_Expt(int x, int y, int a0, int a1, int a2, int b1, int b2,
 		adj_A = adj_A - 200;
     	adj_C = adj_C - 200;
     	sum_AC=0;
-    	sum_A= 0;
+    	sum_A_mat= 0;
     	for(int i = 0;i <= x;i++)
 		{
 			for(int j = 0;j <= y;j++)
@@ -192,20 +191,20 @@ long double *dBvZINB4_Expt(int x, int y, int a0, int a1, int a2, int b1, int b2,
 				l_C_mat[i][j] = l1_c(t1,t2,i,j,adj_C);  
 				l_AC_mat[i][j] = l1_AC(t1,t2,x,y,a0,a1,a2,i,j,adj_A+adj_C);
 				sum_AC += l_AC_mat[i][j];
-				sum_A +=  l_A_mat[i][j]; 
+				sum_A_mat +=  l_A_mat[i][j]; 
 			}
 	    }
 	}
 	long double l_sum =0;
-	cout << sum_AC << " "<< l2_B << endl;
-	l_sum = sum_AC * l1_B + sum_A * (l2_B +  l3_B +  l4_B) * exp(-adj_C);
+	//cout << sum_AC << " "<< l2_B << endl;
+	l_sum = sum_AC * l1_B + sum_A_mat * (l2_B +  l3_B +  l4_B) * exp(-adj_C);
 
 	if (l_sum == 0) 
 	{
 	    adj_sum = -floor(log(sum_AC)*2*1.0/3 + log(l1_B)*2*1.0/3);
 	    //cout << adj_sum<<"adj";
 	    //cout << sum_AC <<"AC"<< adj_sum <<"@"<< l1_B<<"c"<<adj_C<<endl;
-	    l_sum = sum_AC * exp(adj_sum) * l1_B + sum_A * (exp(adj_sum) * (l2_B +  l3_B +  l4_B)) * exp(-adj_C);
+	    l_sum = sum_AC * exp(adj_sum) * l1_B + sum_A_mat * (exp(adj_sum) * (l2_B +  l3_B +  l4_B)) * exp(-adj_C);
   	}
   	long double R0_E1_B,R0_E2_B,R0_E3_B,R0_E4_B,R1_E1_B,R1_E2_B,R1_E3_B,R1_E4_B,R2_E1_B,R2_E2_B,R2_E3_B,R2_E4_B;
 	// expectation components
@@ -275,17 +274,17 @@ long double *dBvZINB4_Expt(int x, int y, int a0, int a1, int a2, int b1, int b2,
 	  log_R2_E = log_R2_E * 1.0 / l_sum; 
 	
 	 double E_E1 = sum_AC * exp(adj_sum) * l1_B;
-	  double E_E2 = sum_A * l2_B *exp(-adj_C + adj_sum);
-	  double E_E3 = sum_A * l3_B *exp(-adj_C + adj_sum);
-	  double E_E4 = sum_A * l4_B *exp(-adj_C + adj_sum);
+	  double E_E2 = sum_A_mat * l2_B *exp(-adj_C + adj_sum);
+	  double E_E3 = sum_A_mat * l3_B *exp(-adj_C + adj_sum);
+	  double E_E4 = sum_A_mat * l4_B *exp(-adj_C + adj_sum);
 	  
 	  double su = E_E1+E_E2+E_E3+E_E4;
-	  E_E1 = E_E1/su;
-	  E_E2 = E_E2/su;
-	  E_E3 = E_E3/su;
-	  E_E4 = E_E4/su;
+	  E_E1 = E_E1*1.0/su;
+	  E_E2 = E_E2*1.0/su;
+	  E_E3 = E_E3*1.0/su;
+	  E_E4 = E_E4*1.0/su;
       //E_E{2} <- E.E/sum(E.E)
-      long double v_E = y==0?0:y + (a0 + a2) * b2 *(E_E2 + E_E3 +E_E4);
+      long double v_E = (y==0?0:y)*1.0 + (a0 + a2) * b2 *(E_E2 + E_E4);
 	  
             /*long double xx = sum_AC * exp(adj_sum) * l1_B * y ; 
             cout<<xx<<"&"<<endl;
@@ -304,11 +303,11 @@ long double *dBvZINB4_Expt(int x, int y, int a0, int a1, int a2, int b1, int b2,
 	  result[4] = log_R0_E;
 	  result[5] = log_R1_E;
 	  result[6] = log_R2_E;
-	  result[7] = v_E;
-	  result[8] = E_E1;
-	  result[9] = E_E2;
-	  result[10] = E_E3;
-	  result[11] = E_E4; 
+	  result[7] = E_E1;
+	  result[8] = E_E2;
+	  result[9] = E_E3;
+	  result[10] = E_E4;
+	  result[11] = v_E; 
 	  return(result);
 	  delete result; 
 	  
@@ -321,7 +320,7 @@ int main()
 	double p1,p2,p3,p4;
 	cin >> x >> y >> a0 >> a1 >> a2>> b1 >> b2 >> p1 >> p2>> p3>>p4;
 	long double *str = new long double[12];
-	string name[12] = {"logdensity","R0_E","R1_E","R2_E", "log_R0_E","log_R1_E","log_R2_E","v_E","E_E1","E_E2","E_E3","E_E4"};
+	string name[12] = {"logdensity","R0_E","R1_E","R2_E", "log_R0_E","log_R1_E","log_R2_E","E_E1","E_E2","E_E3","E_E4","v_E"};
 	clock_t start = clock();
 	str = dBvZINB4_Expt(x,y,a0,a1,a2,b1,b2,p1,p2,p3,p4);
 	    clock_t finish = clock();
@@ -334,5 +333,4 @@ int main()
 
     cout << "finish-start:\t" << (double)(finish - start) / CLOCKS_PER_SEC << 's' << endl;
 	return 0;
-    
- } 
+}
